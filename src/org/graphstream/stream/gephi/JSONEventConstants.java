0ec660271dd3424194ae08c6c8d74417e9bda77e
Min WU <wumalbert@gmail.com>
2012-06-17 00:20:46 +0800

    First task, communicate from Graphstream to Gephi through GSON protocol

---
+++ src/org/graphstream/stream/gephi/JSONEventConstants.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package org.graphstream.stream.gephi;
+
+/**
+ * @author wumalbert
+ *
+ */
+public class JSONEventConstants {
+	
+    public enum Types {
+    	AN("an"),
+    	CN("cn"),
+    	DN("dn"),
+    	AE("ae"),
+    	CE("ce"),
+        DE("de"),
+        CG("cg");
+
+        private String value;
+
+        private Types(String value) {
+                this.value = value;
+        }
+
+        public String value() {
+                return value;
+        }
+    }
+	
+    public enum Fields {    	
+        ID("id"),
+        T("t"), //timestamp
+        SOURCE("source"),
+        TARGET("target"),
+        DIRECTED("directed");
+        private String value;
+
+        private Fields(String value) {
+            this.value = value;
+        }
+
+        public String value() {
+            return value;
+        }
+    };
+}
